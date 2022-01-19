6cc43f5f75ca2ece59bbe3a13b0ee15f6b6d240e
Zhuzilv <vermilionAnd@163.com>
2022-01-20 00:12:11 +0800

    feat easy

---
+++ 27.移除元素.ts
@@ -0,0 +1,12 @@
+function removeElement(nums: number[], val: number): number {
+  let [left,right]=[0,nums.length];
+       while (left < right) {
+      if (nums[left] === val) {
+          nums[left] = nums[right - 1];
+          right--;
+      } else {
+          left++;
+      }
+  }
+  return left;
+};
