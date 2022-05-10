23d68f33cb65857ad560bcdc0336fa1d4fc59aef
vermilionAnd <zhujiahong@growingio>
2022-05-11 00:04:57 +0800

    add: check tool

--- sort/index.js
+++ sort/index.js
@@ -1,9 +1,10 @@
-const arr = [9, 5, 2, 7];
-const exchange =  (arr,l,r)=>{
-  if(l===r) return null
+
+const swap = (arr,l,r)=>{
+  if(l!==r){
   arr[l]^=arr[r];
   arr[r]^=arr[l];
   arr[l]^=arr[r];
+  }
 }
 // 选择排序
 // 9 5 2 7
@@ -13,7 +14,7 @@ const exchange =  (arr,l,r)=>{
 //   for(let i = 0 ;i<array.length;i++){
 //     for(let j = i+1 ;j<array.length;j++){
 //       if(array[j]<array[i]){
-//         exchange(array,i,j)
+//         swap(array,i,j)
 //       }
 //     }
 //   }
@@ -28,7 +29,7 @@ const exchange =  (arr,l,r)=>{
 //     for(let j = i+1 ;j<array.length;j++){
 //        min=array[j]<array[min]?j:min;
 //     }
-//     exchange(array,i,min)
+//     swap(array,i,min)
 //   }
 //   return array;
 // }
@@ -44,7 +45,7 @@ const exchange =  (arr,l,r)=>{
 //   for(let i=arr.length-1;i>=0;i--){
 //     for(let j= 0 ;j<=i;j++){
 //       if(arr[j]>arr[j+1]){
-//         exchange(arr,j,j+1)
+//         swap(arr,j,j+1)
 //       }
 //     }
 //   }
@@ -54,20 +55,60 @@ const exchange =  (arr,l,r)=>{
 
 // 插入排序
 // 9 5 2 7
-// 2 5 9 7 
+// 2 5 9 7
 // 1 ~ n
 // i ~ 0
 function insertSort(array){
   for(let i=1;i<array.length;i++){
     for(let j=i;j>=0 && array[j-1]>array[j];j--){
-        exchange(array,j-1,j)
+        swap(array,j-1,j)
     }
   }
   return array;
 }
-console.log(insertSort(arr))
 // 对数器
-function okSort(arr){
-  return arr.sort((a,b)=>a-b)
+// 自定义长度和随机数
+function arrBuild(maxLenth,maxValue){
+  let arr =new Array(maxLenth).fill(0);
+  for(let i=0;i<arr.length;i++){
+    arr[i] = (Math.random() * maxValue) | 0
+  }
+  return arr;
 }
-console.log(okSort(arr))
+// 验证是否有序
+function isSorted(arr){
+  let max = arr[0]
+  for(let i = 0 ; i<arr.length ;i++){
+    if(max>arr[i+1]){
+      return false;
+    }
+  }
+  return true;
+}
+// copy 1:1
+function copy(arr){
+  let arr2 = new Array(arr.length).fill(0);
+  for(let i = 0 ;i<arr.length ;i++){
+    arr2[i]=arr[i]
+  }
+  return arr2;
+}
+// 大概率比较
+
+function check(){
+  let maxL= 100;
+  let maxValue = 1000;
+  let times = 10000;
+  for(let i = 0 ;i<times ;i++){
+    let arr = arrBuild(maxL,maxValue);
+    let sortArr= insertSort(arr);
+    let copyArr= copy(arr)
+    if(!isSorted(sortArr)){
+      console.log('error')
+      console.log(copyArr)
+    }
+  }
+  console.log('ok')
+}
+
+check();
