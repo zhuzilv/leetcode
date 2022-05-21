91b303d6a84a1a1c0fe8b166417e72284e48b7e6
vermilionAnd <vermilionAnd@gmail.com>
2022-05-21 11:21:57 +0800

    update linkedlist

--- Linked list/reverseLinkedList.js
+++ Linked list/reverseLinkedList.js
@@ -27,13 +27,22 @@
   // cur = next;
 
   // 三指针法，pre，cur，next 
-  let cur =head;
-  let pre = null;
-  while(cur){
-  let next = cur.next; // 存起来
-  cur.next = pre;
-  pre = cur;
-  cur = next;
-  }
- return pre;
+//     let cur =head;
+//     let pre = null;
+//     while(cur){
+//         let next = cur.next; // 存起来
+//         cur.next = pre;
+//         pre = cur;
+//         cur = next;
+//     }
+//    return pre;
+
+  // 递归实现 待理解，先把三指针敲熟
+  if (head == null || head.next == null) return head
+  const p = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return p
+
+
 };
