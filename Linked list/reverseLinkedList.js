46c1c0dc8e803069d59e86fd1847a154743a684c
vermilionAnd <vermilionAnd@gmail.com>
2022-05-21 10:59:23 +0800

    update linkedLIst

--- Linked list/reverseLinkedList.js
+++ Linked list/reverseLinkedList.js
@@ -1,22 +1,39 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
  * }
  */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
- var reverseList = function(head) {
-   // 当前位，上一位，下一位
-  let pre = null,cur = head;
-  while(cur!=null){
-      let next = cur.next; // 把下一位先存起来
-      cur.next = pre; // 指针调换
-      pre = cur; // pre前移
-      cur = next; // cur 前移
-  };
-  return pre;
+
+ function reverseList(head: ListNode | null): ListNode | null {
+  //             p  c   n
+  // null<-1<-2<-5. null
+  // cur = head ;
+  // let pre =null; 
+  // let next = cur.next;
+  // cur.next = pre;
+  // pre = cur;
+  // cur = next;
+  // pre = cur ;
+  // cur = next;
+  //let next = cur.next;
+  // cur.next =pre;
+  // pre = cur;
+  // cur = next;
+
+  // 三指针法，pre，cur，next 
+  let cur =head;
+  let pre = null;
+  while(cur){
+  let next = cur.next; // 存起来
+  cur.next = pre;
+  pre = cur;
+  cur = next;
+  }
+ return pre;
 };
